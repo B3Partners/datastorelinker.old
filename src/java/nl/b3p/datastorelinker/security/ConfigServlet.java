c102abc29e56a1bbfcaedf28a35ad0a94dc6c956
Roy Braam <braam.roy@gmail.com>
2010-09-16 14:33:47 +0000

    Security realm om te laten checken met kaartenbalie. Mocht dit niet werken dan checken of het in de xmlSecurityRealm zit.

---
+++ src/java/nl/b3p/datastorelinker/security/ConfigServlet.java
@@ -0,0 +1,85 @@
+package nl.b3p.datastorelinker.security;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class ConfigServlet extends HttpServlet {
+
+    private static final Log log = LogFactory.getLog(ConfigServlet.class);
+    public static String ANONIEM_ROL = "anoniem";
+    public static String GEBRUIKERS_ROL = "gebruiker";
+    public static String THEMABEHEERDERS_ROL = "themabeheerder";
+    public static String DEMOGEBRUIKERS_ROL = "demogebruiker";
+    public static String BEHEERDERS_ROL = "beheerder";
+    public static String ANONYMOUS_USER = "anoniem";
+    private static String kburl = null;
+    public static String kbWfsConnectieNaam = "Kaartenbalie WFS";
+    
+    /**
+     * http://www.kaartenbalie.nl/kaartenbalie/service/0c462abe62b69b2f05d1e72862f251f6
+     * kburl: http://www.kaartenbalie.nl/kaartenbalie/service/
+     * code:  0c462abe62b69b2f05d1e72862f251f6
+     *
+     * Code kan alleen worden toegevoegd indien de kaartenbalie url eindigt
+     * op een /. Er wordt een / toegevoegd indien dit niet het geval is.
+     *
+     * @param code
+     * @return
+     */
+    public static String createPersonalKbUrl(String code) {
+        if (code != null && code.startsWith("http://")) {
+            return code;
+        }
+        String url = getKbUrl();
+        url = url.trim();
+        if (code != null && code.length()>0) {
+            if (url.lastIndexOf('/') == url.length() - 1) {
+                url += code;
+            } else {
+                url += '/' + code;
+            }
+        }
+        return url;
+    }
+
+    public static String getKbUrl() {
+        return kburl;
+    }
+
+    public static void setKbUrl(String aKburl) {
+        kburl = aKburl;
+    }
+
+    /** Initializes the servlet.
+     */
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+
+        try {
+            String value = config.getInitParameter("kburl");
+            if (value != null && value.length() > 0) {
+                kburl = value;
+            }
+            value = config.getInitParameter("anonymous_user");
+            if (value != null && value.length() > 0) {
+                ANONYMOUS_USER = value;
+            }
+            value = config.getInitParameter("gebruikers_rol");
+            if (value != null && value.length() > 0) {
+                GEBRUIKERS_ROL = value;
+            }
+            value = config.getInitParameter("themabeheerders_rol");
+            if (value != null && value.length() > 0) {
+                THEMABEHEERDERS_ROL = value;
+            }
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+    }
+    // </editor-fold>
+}
---
+++ src/java/nl/b3p/datastorelinker/security/GisPrincipal.java
@@ -0,0 +1,150 @@
+/*
+ * B3P Gisviewer is an extension to Flamingo MapComponents making
+ * it a complete webbased GIS viewer and configuration tool that
+ * works in cooperation with B3P Kaartenbalie.
+ *
+ * Copyright 2006, 2007, 2008 B3Partners BV
+ * 
+ * This file is part of B3P Gisviewer.
+ * 
+ * B3P Gisviewer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * B3P Gisviewer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with B3P Gisviewer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package nl.b3p.datastorelinker.security;
+
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import nl.b3p.wms.capabilities.Layer;
+import nl.b3p.wms.capabilities.Roles;
+import nl.b3p.wms.capabilities.ServiceProvider;
+import nl.b3p.wms.capabilities.Style;
+import nl.b3p.wms.capabilities.StyleDomainResource;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.securityfilter.filter.SecurityRequestWrapper;
+
+public class GisPrincipal implements Principal {
+    public static final String URL_AUTH = "code";
+    private static final Log log = LogFactory.getLog(GisPrincipal.class);
+    private String name;
+    private String password;
+    /*TODO ipv code misschien hele kaartenbalie url??? */
+    private String code;
+    private Set roles;
+    private ServiceProvider sp;
+
+    public GisPrincipal(String name, List roles) {
+        this.name = name;
+        this.roles = new HashSet();
+        this.roles.addAll(roles);
+    }
+
+    public GisPrincipal(String name, String password, String code, ServiceProvider sp) {
+        this.name = name;
+        this.password = password;
+        this.code = code;
+        this.sp = sp;
+        if (sp == null) {
+            return;
+        }
+        this.roles = new HashSet();
+        Set sproles = sp.getAllRoles();
+        if (sproles == null || sproles.isEmpty()) {
+            return;
+        }
+        Iterator it = sproles.iterator();
+        while (it.hasNext()) {
+            Roles role = (Roles) it.next();
+            String sprole = role.getRole();
+            if (sprole != null && sprole.length() > 0) {
+                roles.add(sprole);
+            }
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public boolean isInRole(String role) {
+        if (roles==null){
+            return false;
+        }
+        return roles.contains(role);
+    }
+
+    public Set getRoles() {
+        return roles;
+    }
+
+    public String toString() {
+        return "GisPrincipal[name=" + name + "]";
+    }
+
+    /* TODO: implement equals/hashCode */
+    public ServiceProvider getSp() {
+        return sp;
+    }
+
+    public void setSp(ServiceProvider sp) {
+        this.sp = sp;
+    }   
+
+    public static GisPrincipal getGisPrincipal(HttpServletRequest request) {
+        Principal user = request.getUserPrincipal();
+        if (!(user instanceof GisPrincipal && request instanceof SecurityRequestWrapper)) {
+            return null;
+        }
+        GisPrincipal gp = (GisPrincipal) user;
+
+        String code = request.getParameter(URL_AUTH);
+        if (code != null && code.length() != 0) {
+            if (gp!=null && code.equals(gp.getCode())) {
+                return gp;
+            }
+
+            // user is using different code, so invalidate session and login again
+            HttpSession session = request.getSession();
+            session.invalidate();
+            String url = GisSecurityRealm.createCapabilitiesURL(code);
+            gp = GisSecurityRealm.authenticateHttp(url, ConfigServlet.ANONYMOUS_USER, null, code);
+        }
+
+        // log in found principal
+        if (gp != null) {
+            SecurityRequestWrapper srw = (SecurityRequestWrapper) request;
+            srw.setUserPrincipal(gp);
+            log.debug("Automatic login for user: " + gp.name);
+        }
+        return gp;
+    }
+}
