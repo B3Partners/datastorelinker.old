e6ff16ec695092ae1f4a3aba30eb958311b337ed
Boy de Wit <boydewit@b3partners.nl>
2012-06-05 13:31:34 +0000

    - Service aanroepen met url publish/add/shape/prefix/naam en zip bestand mee posten; - Process wordt nu aangemaakt zonder geldig actieblokken dus tabel is nu de naam van shapefile; - Process wordt na aanmaken gelijk gedraaid (kans soms dus even duren)

--- src/java/nl/b3p/datastorelinker/services/PublishProcessServlet.java
+++ src/java/nl/b3p/datastorelinker/services/PublishProcessServlet.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 import javax.persistence.EntityManager;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -21,6 +22,8 @@ import nl.b3p.datastorelinker.entity.Inout;
 import nl.b3p.datastorelinker.entity.Mail;
 import nl.b3p.datastorelinker.entity.ProcessStatus;
 import nl.b3p.datastorelinker.gui.stripes.ActionsAction;
+import nl.b3p.datastorelinker.util.DataStoreLinkJob;
+import nl.b3p.datastorelinker.util.SchedulerUtils;
 import nl.b3p.datastorelinker.util.ZipUtil;
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileItemFactory;
@@ -30,6 +33,10 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hibernate.Session;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.Trigger;
+import org.quartz.TriggerUtils;
 
 /**
  *
@@ -37,15 +44,18 @@ import org.hibernate.Session;
  */
 public class PublishProcessServlet extends HttpServlet {
 
-    private static final String PARAM_SHAPE = "shape";
-    private static final String PARAM_SDE = "sde";
-    private static final String PARAM_FGDB = "fgdb";
+    private static final String PARAM_METHOD_ADD = "add";
+    
+    private static final String PARAM_SOURCE_SHAPE = "shape";
+    private static final String PARAM_SOURCE_SDE = "sde";
+    private static final String PARAM_SOURCE_FGDB = "fgdb";
     
     private static String outputDatabase;
     private static String uploadFolder;    
     
     private final Log log = LogFactory.getLog(this.getClass());    
 
+    /* Voorbeeld: publish/add/shape/prefix/naam */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws IOException {
 
@@ -60,20 +70,25 @@ public class PublishProcessServlet extends HttpServlet {
         if (pathInfo != null) {
             String method = pathInfo.split("/")[1];
             
-            if (method != null && method.equals(PARAM_SHAPE)) {   
-                String afkorting = pathInfo.split("/")[2];
+            if (method != null && method.equals(PARAM_METHOD_ADD)) {  
+                String type = pathInfo.split("/")[2];
                 
-                processShape(request, afkorting);
+                if (type != null && type.equals(PARAM_SOURCE_SHAPE)) {  
+                    String prefix = pathInfo.split("/")[3];
+                    String naam = pathInfo.split("/")[4];
+                    
+                    String tabelNaam = prefix + "_" + naam;
+                    
+                    processShape(request, tabelNaam);
+                }
             }
         }
         
         PrintWriter out = response.getWriter();
-        out.println("Proces aangemaakt!");
-        
+        out.println("Webservice proces aangemaakt.");       
     }
     
-    private void processShape(HttpServletRequest request, String afk) {
-        
+    private void processShape(HttpServletRequest request, String tabelNaam) {
         /* Shape neerzetten */
         boolean isMultipart = ServletFileUpload.isMultipartContent(request);
         if (isMultipart) {
@@ -112,11 +127,11 @@ public class PublishProcessServlet extends HttpServlet {
                             
                             /* Zip uitpakken in subfolder en dan verwijderen */
                             ZipUtil util = new ZipUtil();
-                            File zipFolder = new File(uploadFolder + afk);
+                            File zipFolder = new File(uploadFolder + tabelNaam);
                             
                             util.extractZip(uploadedFile, zipFolder);
                             
-                            createNewProcess(fileName, afk);
+                            createNewProcess(fileName, tabelNaam);
                             
                         } catch (Exception ex) {
                             log.error("Fout tijdens schrijven shape bestand: ", ex);
@@ -129,15 +144,14 @@ public class PublishProcessServlet extends HttpServlet {
                 
     }
     
-    private void createNewProcess(String fileName, String afk) {
+    private void createNewProcess(String fileName, String identifier) {
         String shapeName = fileName.replaceAll(".zip", ".shp");
-        String tableName = afk + "_" + fileName.replaceAll(".zip", "");
         
-        /* Invoer aanmaken */
+        /* TODO: Als zelfde invoer al bestaat dan niet aanmaken maar dat id gebruiken */
         Inout input = new Inout();        
         input.setDatatype(Inout.Datatype.FILE);
-        input.setFile(uploadFolder + afk + File.separator + shapeName);
-        input.setName("webservice/" + afk + "/" + shapeName); // TODO: Pad naar shp dynamisch instellen
+        input.setFile(uploadFolder + identifier + File.separator + shapeName);
+        input.setName("/webservice/" + identifier + "/" + shapeName); // TODO: Pad naar shp dynamisch instellen
         input.setType(Inout.Type.INPUT);
         input.setOrganizationId(1);
         input.setUserId(1);
@@ -150,10 +164,10 @@ public class PublishProcessServlet extends HttpServlet {
         Long inputId = (Long)session.save(input);
         log.debug("Created input with id: " + inputId);
         
-        /* Uitvoer aanmaken */        
-        Inout output = new Inout();        
+        /* TODO: Als zelfde uitvoer al bestaat dan niet aanmaken maar dat id gebruiken */
+        Inout output = new Inout();      
         output.setDatatype(Inout.Datatype.DATABASE);
-        output.setTableName(tableName);
+        output.setTableName(identifier);
         output.setType(Inout.Type.OUTPUT);
         output.setDatabase(new Database(494L)); // TODO: Aanpassen of ergens op kunnen vragen!
         output.setOrganizationId(1);
@@ -163,11 +177,12 @@ public class PublishProcessServlet extends HttpServlet {
         Long outputId = (Long)session.save(output);
         log.debug("Created output with id: " + outputId);
                
-        /* Proces aanmaken */
+        /* TODO: Eenmalig Mail aanmaken obv web xml instellingen */
+        
         Mail mail = new Mail();
         mail.setFromEmailAddress("dsl@b3partners.nl");
         mail.setSmtpHost("kmail.b3partners.nl");
-        mail.setSubject("Webservice proces " + tableName);
+        mail.setSubject("Webservice proces " + identifier);
         mail.setToEmailAddress("boy@b3p.nl");
         
         session.save(mail);
@@ -175,10 +190,11 @@ public class PublishProcessServlet extends HttpServlet {
         nl.b3p.datastorelinker.entity.Process p = new nl.b3p.datastorelinker.entity.Process();
         
         /* Actieblok klaarzetten */
-        p.setActionsString(createActieBlokString(tableName));
+        // p.setActionsString(createActieBlokString(identifier));
+        p.setActionsString("");
         
         p.setDrop(Boolean.TRUE);
-        p.setName("Webservice proces " + tableName);
+        p.setName("Webservice proces " + identifier);
         p.setWriterType("ActionCombo_GeometrySplitter_Writer");
         p.setInput(input);
         p.setMail(mail); // TODO: Aanpassen of ergens op kunnen vragen!
@@ -198,6 +214,32 @@ public class PublishProcessServlet extends HttpServlet {
         em.getTransaction().commit();
         
         /* Process uitvoeren */
+        execute(processId);
+    }
+    
+    private void execute(Long processId) {
+        EntityManager em = JpaUtilServlet.getThreadEntityManager();
+        Session session = (Session)em.getDelegate();
+        
+        nl.b3p.datastorelinker.entity.Process process = (nl.b3p.datastorelinker.entity.Process)
+                session.get(nl.b3p.datastorelinker.entity.Process.class, processId);
+
+        try {
+            String generatedJobUUID = "job" + UUID.randomUUID().toString();
+            JobDetail jobDetail = new JobDetail(generatedJobUUID, DataStoreLinkJob.class);
+            jobDetail.getJobDataMap().put("processId", process.getId());
+            //jobDetail.getJobDataMap().put("locale", getContext().getLocale());
+            
+            Trigger trigger = TriggerUtils.makeImmediateTrigger(generatedJobUUID, 0, 0);
+            //Trigger trigger = new SimpleTrigger("nowTrigger", new Date());
+            Scheduler scheduler = SchedulerUtils.getScheduler(getServletContext());
+            process.getProcessStatus().setProcessStatusType(ProcessStatus.Type.RUNNING);
+            process.getProcessStatus().setExecutingJobUUID(generatedJobUUID);
+            scheduler.scheduleJob(jobDetail, trigger);
+            
+        } catch(Exception e) {
+            log.error("Fout tijdens uitvoeren webservice process: ", e);
+        }
     }
     
     private String createActieBlokString(String tableName) {
